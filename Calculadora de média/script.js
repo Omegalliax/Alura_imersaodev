21628afdb72fe43ded248578627f76a2113345c2
Vitor Eliel Bezerra Freire <vitoreliel2011@hotmail.com>
2021-09-15 15:17:23 -0300

    Add files via upload

---
+++ Calculadora de média/script.js
@@ -0,0 +1,23 @@
+var nome = "Vitor"
+var notaDoPrimeiroBimestre = 8.2004
+var notaDoSegundoBimestre = 7.3244
+var notaDoTerceiroBimestre = 2.5433
+var notaDoQuartoBimestre = 3.4455
+
+var somaDasNotas = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
+
+var notaFinal = somaDasNotas / 4
+
+
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// calculo de média com fixação em 1 casa decimal toFixed
+//variaveis
+//string
+//console.log
+//operações básicas
+//concatenação
