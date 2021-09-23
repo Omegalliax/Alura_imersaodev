4343880a8ec8b4b05bcecc99d0a1349707a9faf0
Vitor Eliel Bezerra Freire <vitoreliel2011@hotmail.com>
2021-09-23 17:26:15 -0300

    Add files via upload

---
+++ Aluraflix v1/script.js
@@ -0,0 +1,14 @@
+var listaFilmes = [
+    "https://th.bing.com/th/id/OIP.VL0udocsvMAnyopMSmdJhwHaLH?pid=ImgDet&rs=1",
+    "https://i1.wp.com/culturanerdegeek.com.br/wp-content/uploads/2017/04/Vikings-cover-3.jpg",
+    "https://th.bing.com/th/id/R.5495a182b24652fa0aa6f27482e7e99c?rik=iGtMkxvdhdH7NA&pid=ImgRaw&r=0",
+    "https://ptanime.com/wp-content/uploads/2020/04/Capa-Manga-One-Piece-Volume-96-Wano-Arc-e1586190562203.jpg",
+    "https://th.bing.com/th/id/OIP.JND2rybQNLIG1Pom5If99wHaLH?pid=ImgDet&rs=1",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7ea29085-36d5-4e34-b5f8-7ab51a7f26d8/d93hjzq-a2479363-d5ba-4bf7-b279-1be4536c7367.jpg/v1/fill/w_745,h_1073,q_70,strp/fan_made_naruto_volume_70_cover_by_ssjdivine_d93hjzq-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQ0MCIsInBhdGgiOiJcL2ZcLzdlYTI5MDg1LTM2ZDUtNGUzNC1iNWY4LTdhYjUxYTdmMjZkOFwvZDkzaGp6cS1hMjQ3OTM2My1kNWJhLTRiZjctYjI3OS0xYmU0NTM2YzczNjcuanBnIiwid2lkdGgiOiI8PTEwMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.F_QEZ14I63oBqHI_Fba8L4c4WRGEd_C8MKqh-YwUiXs",
+    "https://www.mobygames.com/images/covers/l/384053-the-legend-of-zelda-breath-of-the-wild-limited-edition-nintendo-switch-front-cover.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
